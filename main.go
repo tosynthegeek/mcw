a1fc6fb7e2655cc4a9810bc9302812f83b2d5fe6
tosynthegeek <oluwatosinojedapo@gmail.com>
2024-06-12 07:05:48 +0100

    Bytes to ABI

--- main.go
+++ main.go
@@ -8,23 +8,23 @@ import (
 	"os"
 )
 func main() {
-	// mnemonic := "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about"
-	// passphrase := "mysecretpassword"
-	// privateKey:= "b5b8eda0c8b55a7f9eafd1f15757c65bebe9ecfb929f41a1bef91055c0a6aa9a"
+	mnemonic := "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about"
+	passphrase := "mysecretpassword"
+	privateKey:= "b5b8eda0c8b55a7f9eafd1f15757c65bebe9ecfb929f41a1bef91055c0a6aa9a"
 	abiByte, err:= os.ReadFile("./abi/golem.json")
 	if err != nil {
 		log.Fatal(err.Error())
 	}
-	// abi:= string(abiByte)
-	// fmt.Println("Abi: ", abi)
+	abi:= string(abiByte)
+	fmt.Println("Abi: ", abi)
 
-	// eth.WalletFromMnemonic(mnemonic, passphrase)
+	eth.WalletFromMnemonic(mnemonic, passphrase)
 
-	// fmt.Println("	")
-	// eth.CreateWallet(passphrase)
+	fmt.Println("	")
+	eth.CreateWallet(passphrase)
 
-	// fmt.Println("Address from Private Key...")
-	// eth.GetAddressFromPrivKateKey(privateKey)
+	fmt.Println("Address from Private Key...")
+	eth.GetAddressFromPrivKateKey(privateKey)
 	eth.JsonToABI(abiByte)
 	bp:= types.BalancePayload{
 		Address: "0x0536806df512d6cdde913cf95c9886f65b1d3462",
