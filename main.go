cf5389a307b0366856c069dc950d0528eb0da62b
tosynthegeek <oluwatosinojedapo@gmail.com>
2024-06-12 10:47:22 +0100

    ETH Transfer Update

--- main.go
+++ main.go
@@ -2,44 +2,46 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"math/big"
 	"mcw/eth"
 	types "mcw/types"
+	"os"
 )
 func main() {
-	// mnemonic := "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about"
-	// passphrase := "mysecretpassword"
-	// privateKey:= "b5b8eda0c8b55a7f9eafd1f15757c65bebe9ecfb929f41a1bef91055c0a6aa9a"
-	// abiByte, err:= os.ReadFile("./abi/golem.json")
-	// if err != nil {
-	// 	log.Fatal(err.Error())
-	// }
+	mnemonic := "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about"
+	passphrase := "mysecretpassword"
+	privateKey:= "b5b8eda0c8b55a7f9eafd1f15757c65bebe9ecfb929f41a1bef91055c0a6aa9a"
+	abiByte, err:= os.ReadFile("./abi/golem.json")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 	network:= "Ethereum"
-	// abi:= string(abiByte)
-	// fmt.Println("Abi: ", abi)
+	abi:= string(abiByte)
+	fmt.Println("Abi: ", abi)
 
-	// eth.WalletFromMnemonic(mnemonic, passphrase)
+	eth.WalletFromMnemonic(mnemonic, passphrase)
 
-	// fmt.Println("	")
-	// eth.CreateWallet(passphrase)
+	fmt.Println("	")
+	eth.CreateWallet(passphrase)
 
-	// fmt.Println("Address from Private Key...")
-	// eth.GetAddressFromPrivKateKey(privateKey)
-	// eth.JsonToABI(abiByte)
-	// bp:= types.BalancePayload{
-	// 	Address: "0x0536806df512d6cdde913cf95c9886f65b1d3462",
-	// 	RpcUrl: "https://cloudflare-eth.com",
-	// 	Network: &network,
-	// 	TokenAddress: "0xa74476443119A942dE498590Fe1f2454d7D4aC0d",
-	// 	ABI: abiByte,
-	// }
-	// balance:= eth.GetTokenBalance(bp)
-	// fmt.Println("Address: ", balance.Address)
-	// fmt.Println("Balance: ", balance.Balance.String())
-	// fmt.Printf("Token Address: %s\n", *balance.TokenAddress)
+	fmt.Println("Address from Private Key...")
+	eth.GetAddressFromPrivKateKey(privateKey)
+	eth.JsonToABI(abiByte)
+	bp:= types.BalancePayload{
+		Address: "0x0536806df512d6cdde913cf95c9886f65b1d3462",
+		RpcUrl: "https://cloudflare-eth.com",
+		Network: &network,
+		TokenAddress: "0xa74476443119A942dE498590Fe1f2454d7D4aC0d",
+		ABI: abiByte,
+	}
+	balance:= eth.GetTokenBalance(bp)
+	fmt.Println("Address: ", balance.Address)
+	fmt.Println("Balance: ", balance.Balance.String())
+	fmt.Printf("Token Address: %s\n", *balance.TokenAddress)
 
-	// txHash:= "0x5d49fcaa394c97ec8a9c3e7bd9e8388d420fb050a52083ca52ff24b3b65bc9c2"
-	// eth.GetTxByHash(txHash, bp.RpcUrl)
+	txHash:= "0x5d49fcaa394c97ec8a9c3e7bd9e8388d420fb050a52083ca52ff24b3b65bc9c2"
+	eth.GetTxByHash(txHash, bp.RpcUrl)
 
 	tp:= types.TransferPayload {
 		PrivateKey: " ",
