2b8ddd18bff4d0357dd034bb4b74e08780b2deac
tosynthegeek <121390044+tosynthegeek@users.noreply.github.com>
2024-06-13 08:32:08 +0100

    Update main.go

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	mnemonic := "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about"
 	passphrase := "mysecretpassword"
-	privateKey:= "b5b8eda0c8b55a7f9eafd1f15757c65bebe9ecfb929f41a1bef91055c0a6aa9a"
+	privateKey:= ""
 	abiByte, err:= os.ReadFile("./abi/golem.json")
 	if err != nil {
 		log.Fatal(err.Error())
